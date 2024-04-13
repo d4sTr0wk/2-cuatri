de5caa008c12175f3afba0906b70f224b1086ca1
d4sTr0wk <r56bj1217@gmail.com>
2024-04-13 21:00:55 +0200

    MySS 13/04

--- MSS/Continuos/Tema 1/Estudio/Ejemplo 3/ejemplo3.m
+++ MSS/Continuos/Tema 1/Estudio/Ejemplo 3/ejemplo3.m
@@ -17,7 +17,8 @@ K2 = 2;
 % Condiciones iniciales
 x1_ini = [0; 0];
 x2_ini = [0; 0];
-init_cond = [x1_ini; x2_ini];
+x3_ini = [0; 0];
+init_cond = [x1_ini; x2_ini; x3_ini];
 
 % Intervalo de tiempo
 t_span = [0, 50];
@@ -50,6 +51,7 @@ ylabel('Velocidad (m/s)');
 legend('dx1(t)/dt','dx2(t)/dt');
 
 function dydt = odefcn(t,y,M1,M2,M3,F,B1,B2,B3,B4,K1,K2)
+
     dy1dt = [y(2); 1/M1*(B3*y(6)+K2*y(3)-K1*y(1)-B1*y(2)-B3*y(2))];
     dy2dt = [y(4); 1/M2*(F+B4*y(6)+K2*y(1)-B2*y(4)-K2*y(3)-B4*y(4))];
     dy3dt = [y(6); 1/M3*(B3*y(2)+B4*y(4)-B3*y(6)-B4*y(6))];
--- MSS/Continuos/practicas/practica1/practica1_maxi.m
+++ MSS/Continuos/practicas/practica1/practica1_maxi.m
@@ -1,6 +1,10 @@
 % Modelado y Simulación de Sistemas
 % Práctica 1 - Modelado y Simulación de Ec. Diferenciales
 
+close all;
+clear all;
+clc;
+
 % Parámetros del sistema
 M1 = 8; % Masa del primer objeto (kg)
 M2 = 10; % Masa del segundo objeto (kg)
@@ -9,37 +13,42 @@ K2 = 6; % Cte. elástica (N/m)
 K3 = 7; % Cte. elástica (N/m)
 B1 = 4; % Cte. amortiguación (N·s/m)
 B2 = 5; % Cte. amortiguación (N·s/m)
-f = 1; % Fuerza externa (N)
+F = 1; % Fuerza externa (N)
 
 % Condiciones iniciales
 t_span = [0, 120]; % Intervalo de tiempo
-init_cond = [0; 0]; % Posición y velocidad de la masa para t=0
+x1_ini = [0; 0];
+x2_ini = [0; 0];
+init_cond = [x1_ini; x2_ini]; % Posición y velocidad de la masa para t=0
 
 % Solver Ec. Diferencial
-[t, y] = ode45(@(t, y) odefcn(t,y,M1,f,B1,K1,K2), t_span, init_cond);
+[t, y] = ode45(@(t, y) odefcn(t,y,M1,M2,F,B1,B2,K1,K2,K3), t_span, init_cond);
 
-x_pos = y(:, 1);
-x_vel = y(:, 2);
+x1_pos = y(:, 1);
+x1_vel = y(:, 2);
+x2_pos = y(:, 3);
+x2_vel = y(:, 4);
 
 % Visualización de resultados
 figure;
 grid on;
 subplot(2, 1, 1);
-plot(t, x_pos, 'b-');
+plot(t, x1_pos, 'b-', t, x2_pos, 'r-');
 xlabel('Tiempo (s)');
 ylabel('Posición (m)');
-legend('x1');
+legend('x1_pos', 'x2_pos');
 subplot(2, 1, 2);
-plot(t, x_vel, 'b-');
+plot(t, x1_vel, 'b-', t, x2_vel, 'r-');
 xlabel('Tiempo (s)');
 ylabel('Velocidad (m/s)');
-legend('x1');
+legend('x1_vel', 'x2_vel');
 
 
-function dydt = odefcn(t,y,M1,f,B1,K1,K2)
+function dydt = odefcn(t,y,M1,M2,F,B1,B2,K1,K2,K3)
     % y(1)=x (posicion), y(2)=dx/dt (velocidad)
 
-    % dydt(1) = dx/dt = y(2)
-    % dydt(2) = 
-    dydt = [y(2); f/M1 + K2/M1*y(1) - (K1+K2)/M1*y(1) - B1/M1*y(2)];
+    dy1dt = [y(2); 1/M1*(F + K2*y(3) - (K1+K2)*y(1) - B1*y(2))];
+    dy2dt = [y(4);1/M2*(K2*y(1)-K3*y(3)-K2*y(3)-B2*y(4))];
+       
+    dydt = [dy1dt; dy2dt];
 end
